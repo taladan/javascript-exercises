88be8a55f5691c4cf4bd41def8481696287e361a
D.J. Crosby <taladan@gmail.com>
2023-02-15 15:15:54 -0500

    Solve for all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arrayObject, ...removeArgs) {
+  const args = [...removeArgs];
+  const returnArray = [];
+  for (let i = 0; i < arrayObject.length; i++) {
+    if (args.includes(arrayObject[i])) {
+      continue;
+    } else {
+      returnArray.push(arrayObject[i]);
+    }
+  }
+  return returnArray;
 };
 
 // Do not edit below this line
