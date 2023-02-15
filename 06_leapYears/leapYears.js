a8dad85f07f9c9cd70a9aea78020bea7f84c1603
D.J. Crosby <taladan@gmail.com>
2023-02-15 17:21:08 -0500

    Solve for all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (typeof year === "number") {
+    // is year evenly divisible by 400 or 4 and not 100 it's a leap year
+    if (year % 400 === 0 || (year % 4 === 0 && !(year % 100 === 0))) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
