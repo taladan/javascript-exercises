7fa37c97948555914120f43a04e697a5cc147dbc
D.J. Crosby <taladan@gmail.com>
2023-02-15 17:46:28 -0500

    Solve for all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,26 @@
-const convertToCelsius = function() {
+// # Exercise 07 - tempConversion
+
+// Write two functions that convert temperatures from Fahrenheit to Celsius, and vice versa:
+// ```
+// convertToCelsius(32) // fahrenheit to celsius, should return 0
+
+// convertToFahrenheit(0) // celsius to fahrenheit, should return 32
+// ```
+
+// Because we are human, we want the result temperature to be rounded to one decimal place: i.e., `convertToCelsius(100)` should return `37.8` and not `37.77777777777778`.
+
+// - Try to find by yourself on the Internet how to round a number to 1 decimal place in JavaScript. If you struggle, have a look [here](https://stackoverflow.com/q/7342957/5433628).
+
+const convertToCelsius = function (temperature) {
+  return +(((temperature - 32) * 5) / 9).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  return +(temperature * 1.8 + 32).toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
