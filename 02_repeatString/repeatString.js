15f24c70f4b45efd30108d54dce5b9f1b92d0359
D.J. Crosby <taladan@gmail.com>
2023-02-14 22:19:54 -0500

    Complete first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let newString = "";
+  for (let i = 0; i < num; i++) {
+    newString += string;
+  }
+  return newString;
 };
 
 // Do not edit below this line
