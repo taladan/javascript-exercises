7a3b9e6556c16d8ef4f34fe9836617f4a26ee93a
D.J. Crosby <taladan@gmail.com>
2023-02-24 08:38:24 -0500

    Pass tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // hanging on punctuation.  Likely need regexp here.
+  const noPunctuation = str
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .replace(/\s/g, "");
 
+  const lcStr = noPunctuation.toLowerCase();
+  let value = lcStr.split("").reverse().join("") === lcStr;
+  console.log(lcStr);
+  return value;
 };
 
 // Do not edit below this line
