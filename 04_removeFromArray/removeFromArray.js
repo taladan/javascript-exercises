d556124de67cffe084cf8d17c9c9dbe92ff88bf1
D.J. Crosby <taladan@gmail.com>
2023-02-23 23:16:02 -0500

    Refactor using filter
    Filter is powerful stuff.  Researched using arrow functions and splice
    at first, then realized I was trying to basically filter one function
    against another...changed the direction of my thoughts entirely and
    found this:
    https://stackoverflow.com/questions/34901593/how-to-filter-an-array-from-all-elements-of-another-array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,19 @@
-const removeFromArray = function (arrayObject, ...removeArgs) {
-  const args = [...removeArgs];
-  const returnArray = [];
-  for (let i = 0; i < arrayObject.length; i++) {
-    if (args.includes(arrayObject[i])) {
-      continue;
-    } else {
-      returnArray.push(arrayObject[i]);
-    }
-  }
-  return returnArray;
-};
+// const removeFromArray = function (arrayObject, ...removeArgs) {
+//   const args = [...removeArgs];
+//   const returnArray = [];
+//   for (let i = 0; i < arrayObject.length; i++) {
+//     if (args.includes(arrayObject[i])) {
+//       continue;
+//     } else {
+//       returnArray.push(arrayObject[i]);
+//     }
+//   }
+//   return returnArray;
+// };
+
+function removeFromArray(arrayObject, ...args) {
+  return arrayObject.filter((item) => !args.includes(item));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
