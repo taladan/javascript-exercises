62e9605d8c8c44440eab25002bf64f43c96c9bb4
D.J. Crosby <taladan@gmail.com>
2023-02-24 08:10:02 -0500

    Complete all tests for calculator.js
    So, reduce is definitely the way to go when doing mathematical
    operations on an array where you're looking for a single result.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function (...args) {
+  return args.reduce((accumulator, currentValue) => accumulator + currentValue);
 };
 
-const subtract = function() {
-	
+const subtract = function (...args) {
+  return args.reduce((accumulator, currentValue) => accumulator - currentValue);
 };
 
-const sum = function() {
-	
+const sum = function (args) {
+  let value = 0;
+  if (args.length !== 0) {
+    value = args.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
+  }
+  return value;
 };
 
-const multiply = function() {
-
+const multiply = function (args) {
+  return args.reduce((accumulator, currentValue) => accumulator * currentValue);
 };
 
-const power = function() {
-	
+const power = function (...args) {
+  return args.reduce(
+    (accumulator, currentValue) => accumulator ** currentValue
+  );
 };
 
-const factorial = function() {
-	
+const factorial = function (arg) {
+  let value = 0;
+  if (arg === 0) {
+    value = 1;
+  } else {
+    const args = [];
+    for (i = 1; i <= arg; i++) {
+      args.push(i);
+    }
+    value = args.reduce((a, b) => a * b);
+  }
+  return value;
 };
 
 // Do not edit below this line
@@ -29,5 +48,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
