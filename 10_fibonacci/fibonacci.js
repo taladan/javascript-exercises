8557566e04c0f62b82b74a2cc1cccdd3a4cc4e6d
D.J. Crosby <taladan@gmail.com>
2023-02-24 23:11:44 -0500

    Code a fib seq generator position finder
    This one was more intricate and I ended up
    going to SO to find a solution to it.  I got the
    original code down by looking up a fibonacci seq
    definition and it worked fine.  The refactor
    with `fill()` and `reduce()` ended up taking some
    time to understand and parse through.  I'll likely
    end up having to look this solution up again if
    I end up having to generate a sequence and pull a position
    from it.  I did end up having to tweak the code from
    the solution I found on SO to fit my particular needs.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,37 @@
-const fibonacci = function() {
+const fibonacci = function (position) {
+  /*
+  to solve this I have to have a way to generate a fib sequence
+  then I need to be able to index to 'position' in the sequence
+  and return it.  It must accept integer numbers and strings that
+  are representative of an integer.  It must error on negative numbers
+  */
 
+  //refactor code
+  position = parseInt(position);
+  if (position < 0) {
+    return "OOPS";
+  } else {
+    let fib = new Array(position + 1).fill(1).reduce((acc, cur, curI) => {
+      acc.push(curI <= 1 ? curI : acc[curI - 2] + acc[curI - 1]);
+      return acc;
+    }, []);
+    return fib[position];
+  }
 };
+// working code
+// position = parseInt(position);
+// if (position < 0) {
+//   return "OOPS";
+// } else {
+//     let fib = [1, 1];
+//     for (i = 1; i < position; i++) {
+//       fib.push(fib[i] + fib[i - 1]);
+//       console.log(fib);
+//     }
+//     return fib[position - 1];
+//   }
+// };
 
+// console.log(fibonacci(5));
 // Do not edit below this line
 module.exports = fibonacci;
