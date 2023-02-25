0df41301d416ba4738674a3b77b0574a6429b524
D.J. Crosby <taladan@gmail.com>
2023-02-25 09:04:16 -0500

    Write a function to return the titles of from an array of books

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,28 @@
-const getTheTitles = function() {
+// steps
+// pass array (books) to getTheTitles
+// for each object (book) in array (books), get the 'title' key's value
+// pack that value into a flat array for return
+// return the array that contains only the book titles
+//
+// Catch point:  I struggled with it returning 'undefined' for about an hour
+// or two.  Didn't have the 'return' on the 'books.map()' line.  Simple gotchas
+// are often my downfall.
 
+const getTheTitles = function (books) {
+  return books.map(getTitle);
+  function getTitle(book) {
+    return book.title;
+  }
 };
+
+myBooks = [
+  { title: "Book", author: "bob smith" },
+  { title: "Book2", author: "bob smith" },
+  { title: "Book3", author: "bob smith" },
+  { title: "Book4", author: "bob smith" },
+];
+
+console.log(getTheTitles(myBooks));
 
 // Do not edit below this line
 module.exports = getTheTitles;
